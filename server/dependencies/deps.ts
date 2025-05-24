5811a4cb178e267ceeeca76f080ae5f1bb0cf301
yttiiz <dominique.talis@proton.me>
2025-05-24 00:53:05 -0400

    chore(auth): remove unnecessary dependencies

--- server/dependencies/deps.ts
+++ server/dependencies/deps.ts
@@ -4,7 +4,6 @@ import { oakCors } from "https://deno.land/x/cors/mod.ts";
 import { MongoStore, Session } from "https://deno.land/x/oak_sessions/mod.ts";
 import { load } from "https://deno.land/std@0.202.0/dotenv/mod.ts";
 import {
-  Binary,
   Bson,
   Collection,
   Database,
@@ -16,21 +15,17 @@ import {
   UpdateFilter,
 } from "https://deno.land/x/mongo@v0.31.2/mod.ts";
 import { FindCursor } from "https://deno.land/x/mongo@v0.31.2/src/collection/commands/find.ts";
-import { decode, encode } from "https://deno.land/std@0.188.0/encoding/hex.ts";
 import * as bcrypt from "https://deno.land/x/bcrypt@v0.4.1/mod.ts";
 import * as nodemailer from "npm:nodemailer";
 import * as cheerio from "npm:cheerio";
 
 export {
   bcrypt,
-  Binary,
   Bson,
   cheerio,
   Collection,
   Database,
   Decimal128,
-  decode,
-  encode,
   FindCursor,
   load,
   MongoClient,
