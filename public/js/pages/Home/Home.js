84a681819a6317823241b513352878d2e7ceb9b1
yttiiz <dominique.talis@proton.me>
2025-05-29 22:16:17 -0400

    feat(header): handle header background transition

--- public/js/pages/Home/Home.js
+++ public/js/pages/Home/Home.js
@@ -17,6 +17,7 @@ export class HomePage extends PageBuilder {
     });
 
     this.#renderVisits(visits);
+    this.#handleHeader();
     this.handleHomeLink();
   };
 
@@ -29,6 +30,29 @@ export class HomePage extends PageBuilder {
 
     logoLink.addEventListener("click", (e) => e.preventDefault());
     footerLink.addEventListener("click", (e) => e.preventDefault());
+  };
+
+  #handleHeader = () => {
+    const heroBanner = document.querySelector(".hero-banner");
+    const header = document.querySelector("header[data-header]");
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const [section] = entries;
+
+        header.classList.toggle(
+          "with-bg",
+          !section.isIntersecting,
+        );
+      },
+      {
+        root: globalThis.document,
+        rootMargin: "0px",
+        threshold: 0.1,
+      },
+    );
+
+    observer.observe(heroBanner);
   };
 
   /**
