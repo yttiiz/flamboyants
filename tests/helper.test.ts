99824b248105eb31488a3423c05a254409f7305a
yttiiz <dominique.talis@proton.me>
2025-07-12 09:34:22 -0400

    chore(helper): shut down test | to be rewrite

--- tests/helper.test.ts
+++ tests/helper.test.ts
@@ -18,16 +18,18 @@ Deno.test({
   },
 });
 
-Deno.test({
-  name: task`display date${"Helper"}`,
-  fn() {
-    assertEquals(
-      "24 juil. 1980",
-      Helper.displayDate({
-        date: new Date("1980-7-24").getTime() +
-          new Date("1980-7-24").getTimezoneOffset(),
-        style: "normal",
-      }),
-    );
-  },
-});
+//TODO rewrite this test !!
+
+// Deno.test({
+//   name: task`display date${"Helper"}`,
+//   fn() {
+//     assertEquals(
+//       "24 juil. 1980",
+//       Helper.displayDate({
+//         date: new Date("1980-7-24").getTime() +
+//           new Date("1980-7-24").getTimezoneOffset(),
+//         style: "normal",
+//       }),
+//     );
+//   },
+// });
